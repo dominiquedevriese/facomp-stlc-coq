68755afb3c77f57dd540a9ebf1aa70fa0f1a4eaf
Dominique Devriese <dominique.devriese@cs.kuleuven.be>
2016-08-09 13:11:56 +0200

    Erase for contexts.

--- Compiler/Erase.v
+++ Compiler/Erase.v
@@ -28,6 +28,28 @@ Fixpoint erase (t : SS.Tm) : US.UTm :=
     | SS.fixt _ _ t₁     => US.app UF.ufix (erase t₁)
   end.
 
+Fixpoint erase_pctx (C : SS.PCtx) : US.PCtx :=
+  match C with
+    | SS.phole => phole
+    | SS.pabs τ C => US.pabs (erase_pctx C)
+    | SS.papp₁ C t => US.papp₁ (erase_pctx C) (erase t) 
+    | SS.papp₂ t C => US.papp₂ (erase t) (erase_pctx C)
+    | SS.pite₁ C t₂ t₃ => US.pite₁ (erase_pctx C) (erase t₂) (erase t₃)
+    | SS.pite₂ t₁ C t₃ => US.pite₂ (erase t₁) (erase_pctx C) (erase t₃)
+    | SS.pite₃ t₁ t₂ C => US.pite₃ (erase t₁) (erase t₂) (erase_pctx C)
+    | SS.ppair₁ C t => US.ppair₁ (erase_pctx C) (erase t)
+    | SS.ppair₂ t C => US.ppair₂ (erase t) (erase_pctx C)
+    | SS.pproj₁ C => US.pproj₁ (erase_pctx C)
+    | SS.pproj₂ C => US.pproj₂ (erase_pctx C)
+    | SS.pinl C => US.pinl (erase_pctx C)
+    | SS.pinr C => US.pinr (erase_pctx C)
+    | SS.pcaseof₁ C t₂ t₃ => US.pcaseof₁ (erase_pctx C) (erase t₂) (erase t₃)
+    | SS.pcaseof₂ t₁ C t₃ => US.pcaseof₂ (erase t₁) (erase_pctx C) (erase t₃)
+    | SS.pcaseof₃ t₁ t₂ C => US.pcaseof₃ (erase t₁) (erase t₂) (erase_pctx C)
+    | SS.pseq₁ C t => US.pseq₁ (erase_pctx C) (erase t)
+    | SS.pseq₂ t C => US.pseq₂ (erase t) (erase_pctx C)
+    | SS.pfixt τ₁ τ₂ C => US.papp₂ ufix (erase_pctx C)
+  end.
 
 Lemma erase_scope (t : SS.Tm) (Γ : SS.Env) (τ : SS.Ty) :
   ⟪ Γ ⊢ t : τ ⟫ -> ⟨ dom Γ ⊢ erase t ⟩.
@@ -54,4 +76,46 @@ Proof.
   * exact (IHt2 _ _ H4).
   * apply UF.ufix_ws.
   * exact (IHt _ _ H4).
+Qed.
+
+Lemma erase_pctx_scope (C : SS.PCtx) (Γ₀ Γ : SS.Env) (τ₀ τ : SS.Ty) :
+  ⟪ ⊢ C : Γ₀ , τ₀ → Γ , τ ⟫ → ⟨ ⊢ erase_pctx C : dom Γ₀ → dom Γ ⟩.
+Proof.
+  revert Γ₀ Γ τ₀ τ.
+  induction C; intros Γ₀ Γ τ₀ τ H; inversion H; clear H; simpl; constructor.
+  * exact (IHC _ _ _ _ H3).
+  * exact (IHC _ _ _ _ H2).
+  * exact (erase_scope _ _ _ H4).
+  * exact (erase_scope _ _ _ H2).
+  * exact (IHC _ _ _ _ H4).
+  * exact (IHC _ _ _ _ H3).
+  * exact (erase_scope _ _ _ H5).
+  * exact (erase_scope _ _ _ H6).
+  * exact (erase_scope _ _ _ H3).
+  * exact (IHC _ _ _ _ H5).
+  * exact (erase_scope _ _ _ H6).
+  * exact (erase_scope _ _ _ H3).
+  * exact (erase_scope _ _ _ H5).
+  * exact (IHC _ _ _ _ H6).
+  * exact (IHC _ _ _ _ H2).
+  * exact (erase_scope _ _ _ H4).
+  * exact (erase_scope _ _ _ H2).
+  * exact (IHC _ _ _ _ H4).
+  * exact (IHC _ _ _ _ H1).
+  * exact (IHC _ _ _ _ H1).
+  * exact (IHC _ _ _ _ H1).
+  * exact (IHC _ _ _ _ H1).
+  * exact (IHC _ _ _ _ H3).
+  * exact (erase_scope _ _ _ H5).
+  * exact (erase_scope _ _ _ H6).
+  * exact (erase_scope _ _ _ H3).
+  * exact (IHC _ _ _ _ H5).
+  * exact (erase_scope _ _ _ H6).
+  * exact (erase_scope _ _ _ H3).
+  * exact (erase_scope _ _ _ H5).
+  * exact (IHC _ _ _ _ H6).
+  * exact (IHC _ _ _ _ H2).
+  * exact (erase_scope _ _ _ H4).
+  * apply UF.ufix_ws.
+  * exact (IHC _ _ _ _ H4).
 Qed.
