bac019c4a2c3a7106ed93a996c8666aaacdfb740
Dominique Devriese <dominique.devriese@cs.kuleuven.be>
2016-08-10 15:33:23 +0200

    Define the logical relation...

--- LogRel/LR.v
+++ LogRel/LR.v
@@ -2,7 +2,12 @@ Require Export LogRel.PseudoType.
 Require Import Stlc.SpecSyntax.
 Require Import Stlc.SpecTyping.
 Require Import Utlc.SpecSyntax.
+Require Import Utlc.Inst.
+Require Import UVal.UVal.
+
+
 Require Import Coq.Arith.Wf_nat.
+Require Import Omega.
 
 Module S := Stlc.SpecSyntax.
 Module U := Utlc.SpecSyntax.
@@ -23,24 +28,81 @@ Fixpoint OfTypeUtlc (τ : PTy) (t : U.UTm) : Prop :=
 Definition OfType (τ : PTy) (t₁ : S.Tm) (t₂ : U.UTm) : Prop :=
   OfTypeStlc τ t₁ ∧ OfTypeUtlc τ t₂.
 
+Inductive Direction : Set :=
+| dir_lt
+| dir_gt.
+
 Definition World := nat.
 
+Definition PTRel := PTy -> S.Tm -> U.UTm -> Prop.
+Definition PCRel := PTy -> S.PCtx -> U.PCtx -> Prop.
+
+Definition Obs (d : Direction) (w : World) (ts : S.Tm) (tu : U.UTm) :=
+  match d with
+    | dir_lt => True
+    | dir_gt => True
+  end. 
+
+Definition contrel'
+           (d : Direction) (w : World) (vr' : forall w' : World, w' ≤ w -> PTRel) : PCRel :=
+ fun τ Cs Cu => forall w' (fw : w' ≤ w) ts tu, vr' w' fw τ ts tu -> Obs d w' (S.pctx_app ts Cs) (U.pctx_app tu Cu).
+
+Definition termrel'
+           (d : Direction) (w : World) (vr' : forall w' : World, w' ≤ w -> PTRel) : PTRel :=
+  fun τ ts tu => forall w' (fw : w' ≤ w) Cs Cu, contrel' d w vr' τ Cs Cu -> Obs d w' (S.pctx_app ts Cs) (U.pctx_app tu Cu).
+
+Lemma lt_le {w w' w''} (fw : w' < w) (fw' : w'' ≤ w') : w'' < w.
+Proof.
+  omega.
+Defined.
+
 Definition valrel'' 
-           (w : World) (ind : forall w' : World, w' < w -> forall (τ : PTy) (t₁ : S.Tm) (t₂ : U.UTm), Prop)
-           (τ : PTy)(t₁ : S.Tm) (t₂ : U.UTm) : Prop :=
+           (d : Direction) (w : World) (ind : forall w' : World, w' < w -> PTRel) : PTRel := 
+  fun τ ts tu =>
+  let latervr : PTRel := fun τ ts tu => forall w' (fw : w' < w), ind w' fw τ ts tu in 
+  let laterlatervr : forall w' (fw : w' < w) w'' (fw' : w'' ≤ w'), PTRel := fun w' fw w'' fw' => ind w'' (lt_le fw fw') in 
+  let vrunit : S.Tm -> U.UTm -> Prop := fun ts tu => ts = S.unit ∧ tu = U.unit in
+  let vrbool : S.Tm -> U.UTm -> Prop := fun ts tu => (ts = S.true ∧ tu = U.true) ∨ (ts = S.false ∧ tu = U.false) in
+  let vrprod : PTy -> PTy -> S.Tm -> U.UTm -> Prop := 
+      fun τ₁ τ₂ ts tu => 
+        OfType (ptprod τ₁ τ₂) ts tu ∧
+        exists ts₁ ts₂ tu₁ tu₂, ts = S.pair ts₁ ts₂ ∧ tu = U.pair tu₁ tu₂ ∧
+                                latervr τ₁ ts₁ tu₁ ∧ latervr τ₂ ts₂ tu₂ in
+  let vrsum : PTy -> PTy -> S.Tm -> U.UTm -> Prop := 
+      fun τ₁ τ₂ ts tu => 
+        OfType (ptsum τ₁ τ₂) ts tu ∧
+        (exists ts' tu', ts = S.inl ts' ∧ tu = U.inl tu' ∧ latervr τ₁ ts' tu') ∨
+        (exists ts' tu', ts = S.inr ts' ∧ tu = U.inr tu' ∧ latervr τ₂ ts' tu') in
+  let vrarr : PTy -> PTy -> S.Tm -> U.UTm -> Prop := 
+      fun τ₁ τ₂ ts tu => 
+        OfType (ptarr τ₁ τ₂) ts tu ∧ 
+        exists tsb tub, ts = S.abs (repEmul τ₁) tsb ∧ tu = U.abs tub ∧
+                        forall w' (fw : w' < w) ts' tu',
+                          ind w' fw τ₁ ts' tu' ->
+                          termrel' d w' (laterlatervr w' fw) τ₂ (tsb [beta1 ts']) (tub [beta1 tu']) in
   match τ with
-    | ptarr τ₁ τ₂ => OfType (ptarr τ₁ τ₂) t₁ t₂ ∧ True
-    | ptunit => t₁ = S.unit ∧ t₂ = U.unit
-    | ptbool => (t₁ = S.true ∧ t₂ = U.true) ∨ (t₁ = S.false ∧ t₂ = U.false)
-    | ptprod τ₁ τ₂ => OfType (ptprod τ₁ τ₂) t₁ t₂ ∧ True
-    | ptsum τ₁ τ₂ => OfType (ptsum τ₁ τ₂) t₁ t₂ ∧ True
-    | pEmulDV n p => OfType (pEmulDV n p) t₁ t₂ ∧ True
+    | ptunit => vrunit ts tu
+    | ptbool => vrbool ts tu
+    | ptprod τ₁ τ₂ => vrprod τ₁ τ₂ ts tu
+    | ptsum τ₁ τ₂ => vrsum τ₁ τ₂ ts tu
+    | ptarr τ₁ τ₂ => vrarr τ₁ τ₂ ts tu
+    | pEmulDV n p => OfType (pEmulDV n p) ts tu ∧ 
+                     match n with
+                       | 0 => ts = S.unit ∧ p = imprecise
+                       | S n' => (ts = unkUVal (S n') ∧ p = imprecise) ∨
+                                 (exists ts', ts = inUnit n ts' ∧ vrunit ts' tu) ∨
+                                 (exists ts', ts = inBool n ts' ∧ vrbool ts' tu) ∨
+                                 (exists ts', ts = inProd n ts' ∧ vrprod (pEmulDV n' p) (pEmulDV n' p) ts' tu) ∨
+                                 (exists ts', ts = inSum n ts' ∧ vrsum (pEmulDV n' p) (pEmulDV n' p) ts' tu) ∨
+                                 (exists ts', ts = inArr n ts' ∧ vrarr (pEmulDV n' p) (pEmulDV n' p) ts' tu)
+                     end
   end.
 
-Definition valrel' (w : World) (τ : PTy)(t₁ : S.Tm) (t₂ : U.UTm) : Prop :=
+Definition valrel' (d : Direction) (w : World) (τ : PTy)(t₁ : S.Tm) (t₂ : U.UTm) : Prop :=
   well_founded_induction_type (well_founded_ltof World (fun w => w))
                   (fun w => forall (τ : PTy) (t₁ : S.Tm) (t₂ : U.UTm), Prop)
-                  valrel'' w τ t₁ t₂.
-Definition valrel (τ : PTy) (w : World) (t₁ : S.Tm) (t₂ : U.UTm) : Prop :=
-  valrel' w τ t₁ t₂.
+                  (valrel'' d) w τ t₁ t₂.
+
+Definition valrel (d : Direction) (τ : PTy) (w : World) (t₁ : S.Tm) (t₂ : U.UTm) : Prop :=
+  valrel' d w τ t₁ t₂.
   
--- UVal/UVal.v
+++ UVal/UVal.v
@@ -21,42 +21,40 @@ Proof.
   induction n; eauto using Typing.
 Qed.
 
-Definition inUnit (n : nat) : Tm := abs tunit (inr (inl (var 0))).
+Definition inUnit (n : nat) (t : Tm) := inr (inl t).
 
-Lemma inUnitT {Γ n} : ⟪ Γ ⊢ inUnit n : tunit ⇒ UVal (S n) ⟫.
+Lemma inUnitT {Γ n t} : ⟪ Γ ⊢ t : tunit ⟫ → ⟪ Γ ⊢ inUnit n t : UVal (S n) ⟫.
 Proof.
+  intros tt.
   unfold inUnit. crushTyping.
 Qed.
 
-Definition inBool (n : nat) : Tm := abs tbool (inr (inr (inl (var 0)))).
+Definition inBool (n : nat) (t : Tm): Tm := inr (inr (inl t)).
 
-Lemma inBoolT {Γ n} : ⟪ Γ ⊢ inBool n : tbool ⇒ UVal (S n) ⟫.
+Lemma inBoolT {Γ n t} : ⟪ Γ ⊢ t : tbool ⟫ → ⟪ Γ ⊢ inBool n t : UVal (S n) ⟫.
 Proof.
   unfold inBool. crushTyping.
 Qed.
 
-Definition inProd (n : nat) : Tm := abs (UVal n × UVal n) (inr (inr (inr (inl (var 0))))).
+Definition inProd (n : nat) (t : Tm) : Tm := inr (inr (inr (inl t))).
 
-Lemma inProd_T {Γ n} : ⟪ Γ ⊢ inProd n : (UVal n × UVal n) ⇒ UVal (S n) ⟫.
+Lemma inProd_T {Γ n t} : ⟪ Γ ⊢ t : UVal n × UVal n ⟫ → ⟪ Γ ⊢ inProd n t : UVal (S n) ⟫.
 Proof.
   unfold inProd. crushTyping.
 Qed.
 
-Definition inArr (n : nat) : Tm := abs (UVal n ⇒ UVal n) (inr (inr (inr (inr (inl (var 0)))))).
+Definition inArr (n : nat) (t : Tm) : Tm := inr (inr (inr (inr (inl t)))).
 
-Lemma inArr_T {Γ n} : ⟪ Γ ⊢ inArr n : (UVal n ⇒ UVal n) ⇒ UVal (S n) ⟫.
+Lemma inArr_T {Γ n t} : ⟪ Γ ⊢ t : UVal n ⇒ UVal n ⟫ → ⟪ Γ ⊢ inArr n t : UVal (S n) ⟫.
 Proof.
   unfold inArr. crushTyping.
 Qed.
 
-Definition inSum (n : nat) : Tm := abs (UVal n ⊎ UVal n) (inr (inr (inr (inr (inr (var 0)))))).
+Definition inSum (n : nat) (t : Tm) : Tm := inr (inr (inr (inr (inr t)))).
 
-Lemma inSum_T {Γ n} : ⟪ Γ ⊢ inSum n : (UVal n ⊎ UVal n) ⇒ UVal (S n) ⟫.
+Lemma inSum_T {Γ n t} : ⟪ Γ ⊢ t : UVal n ⊎ UVal n ⟫ → ⟪ Γ ⊢ inSum n t : UVal (S n) ⟫.
 Proof.
   unfold inSum. crushTyping.
 Qed.
 
 
-(* Local Variables: *)
-(* coq-load-path: (("." "UVal") ("../Stlc" "Stlc")) *)
-(* End: *)
