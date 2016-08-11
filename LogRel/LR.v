e6f0732de68763ea1fd393f1a06bcd228b5fe59e
Dominique Devriese <dominique.devriese@cs.kuleuven.be>
2016-08-11 11:33:15 +0200

    Simplify the definition of the value relation a little.

--- LogRel/LR.v
+++ LogRel/LR.v
@@ -61,47 +61,52 @@ Proof.
   omega.
 Defined.
 
+Definition prod_rel (R₁ R₂ : S.Tm -> U.UTm -> Prop) : S.Tm -> U.UTm -> Prop :=
+  fun ts tu => 
+    exists ts₁ ts₂ tu₁ tu₂,
+      ts = S.pair ts₁ ts₂ ∧ tu = U.pair tu₁ tu₂ ∧
+      R₁ ts₁ tu₁ ∧ R₂ ts₂ tu₂.
+Definition sum_rel (R₁ R₂ : S.Tm -> U.UTm -> Prop) : S.Tm -> U.UTm -> Prop :=
+  fun ts tu => 
+    (exists ts' tu', ts = S.inl ts' ∧ tu = U.inl tu' ∧ R₁ ts' tu') ∨
+    (exists ts' tu', ts = S.inr ts' ∧ tu = U.inr tu' ∧ R₂ ts' tu').
+
 Definition valrel'' 
            (d : Direction) (w : World) (ind : forall w' : World, w' < w -> PTRel) : PTRel := 
   fun τ ts tu =>
-  let latervr : PTRel := fun τ ts tu => forall w' (fw : w' < w), ind w' fw τ ts tu in 
-  let laterlatervr : forall w' (fw : w' < w) w'' (fw' : w'' ≤ w'), PTRel := fun w' fw w'' fw' => ind w'' (lt_le fw fw') in 
-  let vrunit : S.Tm -> U.UTm -> Prop := fun ts tu => ts = S.unit ∧ tu = U.unit in
-  let vrbool : S.Tm -> U.UTm -> Prop := fun ts tu => (ts = S.true ∧ tu = U.true) ∨ (ts = S.false ∧ tu = U.false) in
-  let vrprod : PTy -> PTy -> S.Tm -> U.UTm -> Prop := 
-      fun τ₁ τ₂ ts tu => 
-        OfType (ptprod τ₁ τ₂) ts tu ∧
-        exists ts₁ ts₂ tu₁ tu₂, ts = S.pair ts₁ ts₂ ∧ tu = U.pair tu₁ tu₂ ∧
-                                latervr τ₁ ts₁ tu₁ ∧ latervr τ₂ ts₂ tu₂ in
-  let vrsum : PTy -> PTy -> S.Tm -> U.UTm -> Prop := 
-      fun τ₁ τ₂ ts tu => 
-        OfType (ptsum τ₁ τ₂) ts tu ∧
-        (exists ts' tu', ts = S.inl ts' ∧ tu = U.inl tu' ∧ latervr τ₁ ts' tu') ∨
-        (exists ts' tu', ts = S.inr ts' ∧ tu = U.inr tu' ∧ latervr τ₂ ts' tu') in
-  let vrarr : PTy -> PTy -> S.Tm -> U.UTm -> Prop := 
-      fun τ₁ τ₂ ts tu => 
-        OfType (ptarr τ₁ τ₂) ts tu ∧ 
-        exists tsb tub, ts = S.abs (repEmul τ₁) tsb ∧ tu = U.abs tub ∧
-                        forall w' (fw : w' < w) ts' tu',
-                          ind w' fw τ₁ ts' tu' ->
-                          termrel' d w' (laterlatervr w' fw) τ₂ (tsb [beta1 ts']) (tub [beta1 tu']) in
-  match τ with
-    | ptunit => vrunit ts tu
-    | ptbool => vrbool ts tu
-    | ptprod τ₁ τ₂ => vrprod τ₁ τ₂ ts tu
-    | ptsum τ₁ τ₂ => vrsum τ₁ τ₂ ts tu
-    | ptarr τ₁ τ₂ => vrarr τ₁ τ₂ ts tu
-    | pEmulDV n p => OfType (pEmulDV n p) ts tu ∧ 
-                     match n with
-                       | 0 => ts = S.unit ∧ p = imprecise
-                       | S n' => (ts = unkUVal (S n') ∧ p = imprecise) ∨
-                                 (exists ts', ts = inUnit n ts' ∧ vrunit ts' tu) ∨
-                                 (exists ts', ts = inBool n ts' ∧ vrbool ts' tu) ∨
-                                 (exists ts', ts = inProd n ts' ∧ vrprod (pEmulDV n' p) (pEmulDV n' p) ts' tu) ∨
-                                 (exists ts', ts = inSum n ts' ∧ vrsum (pEmulDV n' p) (pEmulDV n' p) ts' tu) ∨
-                                 (exists ts', ts = inArr n ts' ∧ vrarr (pEmulDV n' p) (pEmulDV n' p) ts' tu)
-                     end
-  end.
+    OfType τ ts tu ∧
+    let latervr : PTRel := fun τ ts tu => forall w' (fw : w' < w), ind w' fw τ ts tu in 
+    let laterlatervr : forall w' (fw : w' < w) w'' (fw' : w'' ≤ w'), PTRel := fun w' fw w'' fw' => ind w'' (lt_le fw fw') in 
+    let vrunit : S.Tm -> U.UTm -> Prop := fun ts tu => ts = S.unit ∧ tu = U.unit in
+    let vrbool : S.Tm -> U.UTm -> Prop := fun ts tu => (ts = S.true ∧ tu = U.true) ∨ (ts = S.false ∧ tu = U.false) in
+    let vrprod : PTy -> PTy -> S.Tm -> U.UTm -> Prop := 
+        fun τ₁ τ₂ ts tu => 
+          prod_rel (latervr τ₁) (latervr τ₂) ts tu in
+    let vrsum : PTy -> PTy -> S.Tm -> U.UTm -> Prop := 
+        fun τ₁ τ₂ ts tu => 
+          sum_rel (latervr τ₁) (latervr τ₂) ts tu in
+    let vrarr : PTy -> PTy -> S.Tm -> U.UTm -> Prop := 
+        fun τ₁ τ₂ ts tu => 
+          exists tsb tub, ts = S.abs (repEmul τ₁) tsb ∧ tu = U.abs tub ∧
+                          forall w' (fw : w' < w) ts' tu',
+                            ind w' fw τ₁ ts' tu' ->
+                            termrel' d w' (laterlatervr w' fw) τ₂ (tsb [beta1 ts']) (tub [beta1 tu']) in
+    match τ with
+      | ptunit => vrunit ts tu
+      | ptbool => vrbool ts tu
+      | ptprod τ₁ τ₂ => vrprod τ₁ τ₂ ts tu
+      | ptsum τ₁ τ₂ => vrsum τ₁ τ₂ ts tu
+      | ptarr τ₁ τ₂ => vrarr τ₁ τ₂ ts tu
+      | pEmulDV n p => match n with
+                         | 0 => ts = S.unit ∧ p = imprecise
+                         | S n' => (ts = unkUVal (S n') ∧ p = imprecise) ∨
+                                   (exists ts', ts = inUnit n ts' ∧ vrunit ts' tu) ∨
+                                   (exists ts', ts = inBool n ts' ∧ vrbool ts' tu) ∨
+                                   (exists ts', ts = inProd n ts' ∧ vrprod (pEmulDV n' p) (pEmulDV n' p) ts' tu) ∨
+                                   (exists ts', ts = inSum n ts' ∧ vrsum (pEmulDV n' p) (pEmulDV n' p) ts' tu) ∨
+                                   (exists ts', ts = inArr n ts' ∧ vrarr (pEmulDV n' p) (pEmulDV n' p) ts' tu)
+                       end
+    end.
 
 Definition valrel' (d : Direction) (w : World) (τ : PTy)(t₁ : S.Tm) (t₂ : U.UTm) : Prop :=
   well_founded_induction_type (well_founded_ltof World (fun w => w))
