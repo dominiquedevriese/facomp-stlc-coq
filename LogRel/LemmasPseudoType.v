c2b940a7c8f56f0ce59e80a56376b5691e4987ab
Steven Keuchel <steven.keuchel@cs.kuleuven.be>
2016-08-28 09:05:42 +0200

    Prove OfType inversions

--- LogRel/LemmasPseudoType.v
+++ LogRel/LemmasPseudoType.v
@@ -1,55 +1,18 @@
+Require Import Stlc.CanForm.
 Require Import Stlc.LemmasTyping.
 Require Export LogRel.PseudoType.
+Require Import UVal.UVal.
 
 Section OfType.
 
-  Local Ltac solveOfType :=
+  Local Ltac crush :=
     unfold OfType, OfTypeStlc; intros;
     repeat
-      match goal with
-        | [ |- _ ∧ _ ] => split
-      end;
-    crushTyping.
-
-  Lemma OfType_unit : OfType ptunit S.unit U.unit.
-  Proof. solveOfType. Qed.
-
-  Lemma OfType_true : OfType ptbool S.true U.true.
-  Proof. solveOfType. Qed.
-
-  Lemma OfType_false : OfType ptbool S.false U.false.
-  Proof. solveOfType. Qed.
-
-  Lemma OfType_inl {τ₁ τ₂ ts tu} :
-    OfType τ₁ ts tu →
-    OfType (ptsum τ₁ τ₂) (S.inl ts) (U.inl tu).
-  Proof. solveOfType. Qed.
-
-  Lemma OfType_inr {τ₁ τ₂ ts tu} :
-    OfType τ₂ ts tu →
-    OfType (ptsum τ₁ τ₂) (S.inr ts) (U.inr tu).
-  Proof. solveOfType. Qed.
-
-  Lemma OfType_pair {τ₁ τ₂ ts₁ ts₂ tu₁ tu₂} :
-    OfType τ₁ ts₁ tu₁ →
-    OfType τ₂ ts₂ tu₂ →
-    OfType (ptprod τ₁ τ₂) (S.pair ts₁ ts₂) (U.pair tu₁ tu₂).
-  Proof. solveOfType. Qed.
-
-  Lemma OfType_lambda {τ₁ τ₂ tsb tub} :
-    ⟪ empty ⊢ S.abs (repEmul τ₁) tsb : repEmul τ₁ ⇒ repEmul τ₂ ⟫ →
-    OfType (ptarr τ₁ τ₂) (S.abs (repEmul τ₁) tsb) (U.abs tub).
-  Proof. solveOfType. Qed.
-
-
-  Lemma OfTypeUtlc_implies_Value {τ tu} :
-    OfTypeUtlc τ tu →
-    U.Value tu.
-  Proof.
-    revert tu.
-    induction τ; cbn; intros;
-    repeat
-      (try
+      (subst;
+       stlcCanForm;
+       crushTyping;
+       destruct_conjs;
+       repeat
          match goal with
            | H: False |- _ => elim H
            | H: True  |- _ => clear H
@@ -57,8 +20,90 @@ Section OfType.
            | H: match ?tu with _ => _ end |- _ =>
              destruct tu eqn: ?; cbn in H
            | H: _ ∨ _ |- _ => destruct  H
-         end;
-       cbn; subst; auto).
+           | [ |- _ ∧ _ ] => split
+         end); eauto 20.
+
+  Lemma OfType_unit : OfType ptunit S.unit U.unit.
+  Proof. crush. Qed.
+
+  Lemma OfType_true : OfType ptbool S.true U.true.
+  Proof. crush. Qed.
+
+  Lemma OfType_false : OfType ptbool S.false U.false.
+  Proof. crush. Qed.
+
+  Lemma OfType_inl {τ₁ τ₂ ts tu} :
+    OfType τ₁ ts tu →
+    OfType (ptsum τ₁ τ₂) (S.inl ts) (U.inl tu).
+  Proof. crush. Qed.
+
+  Lemma OfType_inr {τ₁ τ₂ ts tu} :
+    OfType τ₂ ts tu →
+    OfType (ptsum τ₁ τ₂) (S.inr ts) (U.inr tu).
+  Proof. crush. Qed.
+
+  Lemma OfType_pair {τ₁ τ₂ ts₁ ts₂ tu₁ tu₂} :
+    OfType τ₁ ts₁ tu₁ →
+    OfType τ₂ ts₂ tu₂ →
+    OfType (ptprod τ₁ τ₂) (S.pair ts₁ ts₂) (U.pair tu₁ tu₂).
+  Proof. crush. Qed.
+
+  Lemma OfType_lambda {τ₁ τ₂ tsb tub} :
+    ⟪ empty ⊢ S.abs (repEmul τ₁) tsb : repEmul τ₁ ⇒ repEmul τ₂ ⟫ →
+    OfType (ptarr τ₁ τ₂) (S.abs (repEmul τ₁) tsb) (U.abs tub).
+  Proof. crush. Qed.
+
+  Lemma OfType_inversion_ptunit {ts tu} :
+    OfType ptunit ts tu →
+    ts = S.unit ∧ tu = U.unit.
+  Proof. crush. Qed.
+
+  Lemma OfType_inversion_ptbool {ts tu} :
+    OfType ptbool ts tu →
+    ts = S.true ∧ tu = U.true ∨
+    ts = S.true ∧ tu = U.false ∨
+    ts = S.false ∧ tu = U.true ∨
+    ts = S.false ∧ tu = U.false.
+  Proof. crush. Qed.
+
+  Lemma OfType_inversion_ptsum {τ₁ τ₂ ts tu} :
+    OfType (ptsum τ₁ τ₂) ts tu →
+    ∃ ts' tu',
+      ts = S.inl ts' ∧ tu = U.inl tu' ∧ OfType τ₁ ts' tu' ∨
+      ts = S.inl ts' ∧ tu = U.inr tu' ∨
+      ts = S.inr ts' ∧ tu = U.inl tu' ∨
+      ts = S.inr ts' ∧ tu = U.inr tu' ∧ OfType τ₂ ts' tu'.
+  Proof. crush. Qed.
+
+  Lemma OfType_inversion_ptprod {τ₁ τ₂ ts tu} :
+    OfType (ptprod τ₁ τ₂) ts tu →
+    ∃ ts₁ tu₁ ts₂ tu₂,
+      ts = S.pair ts₁ ts₂ ∧
+      tu = U.pair tu₁ tu₂ ∧
+      OfType τ₁ ts₁ tu₁ ∧
+      OfType τ₂ ts₂ tu₂.
+  Proof. crush. Qed.
+
+  Lemma OfType_inversion_ptarr {τ₁ τ₂ ts tu} :
+    OfType (ptarr τ₁ τ₂) ts tu →
+    ∃ ts' tu',
+      ts = S.abs (repEmul τ₁) ts' ∧
+      tu = U.abs tu' ∧
+      ⟪ empty ▻ repEmul τ₁ ⊢ ts' : repEmul τ₂ ⟫.
+  Proof. crush. Qed.
+
+  Lemma OfType_inversion_pEmulDV {n p ts tu} :
+    OfType (pEmulDV n p) ts tu →
+    S.Value ts ∧ U.Value tu ∧
+    ⟪ empty ⊢ ts : UVal n ⟫.
+  Proof. crush. Qed.
+
+  Lemma OfTypeUtlc_implies_Value {τ tu} :
+    OfTypeUtlc τ tu →
+    U.Value tu.
+  Proof.
+    revert tu.
+    induction τ; crush.
   Qed.
 
   Lemma OfType_implies_Value {τ ts tu} :
@@ -75,6 +120,12 @@ End OfType.
 Ltac crushOfType :=
   repeat
     match goal with
+      | H: OfType ptunit _ _ |- _ => apply OfType_inversion_ptunit in H
+      | H: OfType ptbool _ _ |- _ => apply OfType_inversion_ptbool in H
+      | H: OfType (ptsum _ _) _ _ |- _ => apply OfType_inversion_ptsum in H
+      | H: OfType (ptprod _ _) _ _ |- _ => apply OfType_inversion_ptprod in H
+      | H: OfType (ptarr _ _) _ _ |- _ => apply OfType_inversion_ptarr in H
+      | H: OfType (pEmulDV _ _) _ _ |- _ => apply OfType_inversion_pEmulDV in H
       | [ |- OfType ptunit S.unit U.unit ] => apply OfType_unit
       | [ |- OfType ptbool S.true U.true ] => apply OfType_true
       | [ |- OfType ptbool S.false U.false ] => apply OfType_false
